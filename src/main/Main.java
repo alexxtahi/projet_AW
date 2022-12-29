3ddfc684343f7ad97a56fae467f5bb5d7af588de
AlexTAHI <62557152+alexxtahi@users.noreply.github.com>
2022-12-29 11:14:35 +0100

    Commit avant le début

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,5 @@
 package main;
+
 import ressources.Affichage;
 import ressources.Chemins;
 
@@ -8,20 +9,20 @@ import java.io.InterruptedIOException;
 import librairies.SelecteurDeFichier;
 import librairies.StdDraw;
 
-
 public class Main {
 
 	final static boolean SAUTER_SELECTION_CARTE = false; // mettre a false pour avoir l'outil de selection de carte
-	
+
 	public static void main(String[] args) throws IOException {
-		try{
+		try {
 			String cheminCarte = selectionCarte();
-			Jeu jeu= new Jeu(cheminCarte);
+			Jeu jeu = new Jeu(cheminCarte);
 			jeu.initialDisplay();
-			StdDraw.show();  //StdDraw est utilise en mode buffer pour fluidifier l'affichage: utiliser StdDraw.show() pour afficher ce qui est dans le buffer
+			StdDraw.show(); // StdDraw est utilise en mode buffer pour fluidifier l'affichage: utiliser
+							// StdDraw.show() pour afficher ce qui est dans le buffer
 			while (!jeu.isOver()) {
 				jeu.update();
- 			}
+			}
 
 		} catch (Exception e) {
 			System.out.println("Erreur lors de l'execution du jeu : " + e.getMessage());
@@ -29,14 +30,11 @@ public class Main {
 	}
 
 	private static String selectionCarte() throws IOException {
-		try
-		{
+		try {
 			SelecteurDeFichier selecteur = new SelecteurDeFichier(Chemins.getDossierCartes());
 			return selecteur.selectFile();
-		}
-		catch (InterruptedIOException excep)
-		{
+		} catch (InterruptedIOException excep) {
 			throw new IOException("Selection annulée, jeu non lancé.");
 		}
 	}
-} 
+}
