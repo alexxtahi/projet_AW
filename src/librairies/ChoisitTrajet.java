d9e151d8241cc4c37e100819e4f9a81903f2b2de
AlexTAHI <62557152+alexxtahi@users.noreply.github.com>
2023-01-14 16:09:58 +0100

    Intégration des couts de déplacement - Partie 3

--- src/librairies/ChoisitTrajet.java
+++ src/librairies/ChoisitTrajet.java
@@ -8,49 +8,51 @@ public class ChoisitTrajet extends Etat {
 
     public ChoisitTrajet(Unite uniteAdeplacer, int curseurX, int curseurY) {
         super(uniteAdeplacer, curseurX, curseurY);
-        System.out.println("Déplacement d'une unité");
+        System.out.println("Déplacement de l'unité " + uniteAdeplacer.getClass().getName() + " - Pts de déplacement: "
+                + uniteAdeplacer.getPointsDep());
     }
 
     private boolean canUniteMove(Case destination) {
         int coutDuDep = Deplacement.getCoutDuDep(super.uniteAdeplacer.getMoyenDeDep(),
                 destination.getTerrain().getType());
-        return (super.uniteAdeplacer.getPointsDep() - coutDuDep > 0);
+        System.out.println("Deps restants: " + super.uniteAdeplacer.getPointsDep());
+        return (super.uniteAdeplacer.getPointsDep() - coutDuDep >= 0);
     }
 
     @Override
-    public Etat actionHaut(Case destination) {
-        super.actionHaut(destination);
-        if (canUniteMove(destination))
-            ajouteDeplacement(destination, Chemins.DIRECTION_BAS, Chemins.DIRECTION_HAUT);
+    public Etat actionHaut(Case[][] carte) {
+        super.actionHaut(carte);
+        if (canUniteMove(super.destinationDuCurseur))
+            ajouteDeplacement(super.destinationDuCurseur, Chemins.DIRECTION_BAS, Chemins.DIRECTION_HAUT);
         return this;
     }
 
     @Override
-    public Etat actionBas(Case destination) {
-        super.actionBas(destination);
-        if (canUniteMove(destination))
-            ajouteDeplacement(destination, Chemins.DIRECTION_HAUT, Chemins.DIRECTION_BAS);
+    public Etat actionBas(Case[][] carte) {
+        super.actionBas(carte);
+        if (canUniteMove(super.destinationDuCurseur))
+            ajouteDeplacement(super.destinationDuCurseur, Chemins.DIRECTION_HAUT, Chemins.DIRECTION_BAS);
         return this;
     }
 
     @Override
-    public Etat actionGauche(Case destination) {
-        super.actionGauche(destination);
-        if (canUniteMove(destination))
-            ajouteDeplacement(destination, Chemins.DIRECTION_DROITE, Chemins.DIRECTION_GAUCHE);
+    public Etat actionGauche(Case[][] carte) {
+        super.actionGauche(carte);
+        if (canUniteMove(super.destinationDuCurseur))
+            ajouteDeplacement(super.destinationDuCurseur, Chemins.DIRECTION_DROITE, Chemins.DIRECTION_GAUCHE);
         return this;
     }
 
     @Override
-    public Etat actionDroite(Case destination) {
-        super.actionDroite(destination);
-        if (canUniteMove(destination))
-            ajouteDeplacement(destination, Chemins.DIRECTION_GAUCHE, Chemins.DIRECTION_DROITE);
+    public Etat actionDroite(Case[][] carte) {
+        super.actionDroite(carte);
+        if (canUniteMove(super.destinationDuCurseur))
+            ajouteDeplacement(super.destinationDuCurseur, Chemins.DIRECTION_GAUCHE, Chemins.DIRECTION_DROITE);
         return this;
     }
 
     @Override
-    public Etat actionEntree(Case caseActuelle, int indexJoueurActif) {
+    public Etat actionEntree(Case[][] carte, int indexJoueurActif) {
         // if ()
         return new NavigationLibre(getCurseurX(), getCurseurY());
     }
