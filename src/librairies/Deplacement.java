891cec3ab474ddb38701421d40fcc1251ba36252
yamadior <97664370+yamadior@users.noreply.github.com>
2023-01-15 14:32:25 +0100

    javadoc_hie

--- src/librairies/Deplacement.java
+++ src/librairies/Deplacement.java
@@ -6,56 +6,76 @@ import ressources.Affichage;
 import ressources.Chemins;
 
 public class Deplacement {
-    // private Case destination;
-    private String image;
-    private String debut;
-    private String fin;
-    private int x;
-    private int y;
+	// private Case destination;
+	private String image;
+	private String debut;
+	private String fin;
+	private int x;
+	private int y;
 
-    public Deplacement(Case destination, String debut, String fin, int x, int y) {
-        // this.destination = destination;
-        this.image = Chemins.getCheminFleche(debut, fin);
-        this.debut = debut;
-        this.fin = fin;
-        this.x = x;
-        this.y = y;
-    }
+	public Deplacement(Case destination, String debut, String fin, int x, int y) {
+		// this.destination = destination;
+		this.image = Chemins.getCheminFleche(debut, fin);
+		this.debut = debut;
+		this.fin = fin;
+		this.x = x;
+		this.y = y;
+	}
 
-    protected static int getCoutDuDep(String moyenDeDep, String typeDeTerrain) {
-        if (moyenDeDep.equals("Aerien")) {
-            return 1;
-        } else if ((moyenDeDep.equals("Pieds") || moyenDeDep.equals("Chenilles"))
-                && (typeDeTerrain.equals("Plaine") || !Jeu.dicoTypesTerrain.contains(typeDeTerrain))) {
-            return 1;
-        } else if (moyenDeDep.equals("Pieds") && typeDeTerrain.equals("Foret")) {
-            return 1;
-        } else if (moyenDeDep.equals("Pieds") && typeDeTerrain.equals("Montagne")) {
-            return 2;
-        } else if (moyenDeDep.equals("Chenilles") && typeDeTerrain.equals("Foret")) {
-            return 2;
-        }
-        return -1;
-    }
+	/**
+	 * Calcule et renvoie le coût du déplacement en fonction du moyen de déplacement
+	 * et du type de l'unité terrain
+	 * 
+	 * @param moyenDeDep
+	 * @param typeDeTerrain
+	 * @return moyen de locomotion
+	 * 
+	 */
+	protected static int getCoutDuDep(String moyenDeDep, String typeDeTerrain) {
+		if (moyenDeDep.equals("Aerien")) {
+			return 1;
+		} else if ((moyenDeDep.equals("Pieds") || moyenDeDep.equals("Chenilles"))
+				&& (typeDeTerrain.equals("Plaine") || !Jeu.dicoTypesTerrain.contains(typeDeTerrain))) {
+			return 1;
+		} else if (moyenDeDep.equals("Pieds") && typeDeTerrain.equals("Foret")) {
+			return 1;
+		} else if (moyenDeDep.equals("Pieds") && typeDeTerrain.equals("Montagne")) {
+			return 2;
+		} else if (moyenDeDep.equals("Chenilles") && typeDeTerrain.equals("Foret")) {
+			return 2;
+		}
+		return -1;
+	}
 
-    public int[] getPosition() {
-        return new int[] { x, y };
-    }
+	public int[] getPosition() {
+		return new int[] { x, y };
+	}
 
-    public String getDebut() {
-        return debut;
-    }
+	public String getDebut() {
+		return debut;
+	}
 
-    public String getFin() {
-        return fin;
-    }
+	public String getFin() {
+		return fin;
+	}
 
-    public void setFin(String nouvelleFin) {
-        fin = nouvelleFin;
-        image = Chemins.getCheminFleche(debut, nouvelleFin);
-    }
+	/**
+	 * modifie l'ancienne fin pour afficher la nouvelle fin
+	 * 
+	 * @param nouvelleFin la nouvelle position de l'unité
+	 * 
+	 */
 
-    public void affiche() {
-        Affichage.dessineImageDansCase(x, y, image);
-    }
+	public void setFin(String nouvelleFin) {
+		fin = nouvelleFin;
+		image = Chemins.getCheminFleche(debut, nouvelleFin);
+	}
+
+	/**
+	 * affiche l'image correspondant au déplacéments
+	 * 
+	 */
+	public void affiche() {
+		Affichage.dessineImageDansCase(x, y, image);
+	}
 }
--- src/main/Joueur.java
+++ src/main/Joueur.java
@@ -1,36 +1,37 @@
 package main;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import cases.Propriete;
-import unites.Unite;
-
 public class Joueur {
 
-    private int id;
-    private int argent;
-    private int nbProp;
+	private int id;
+	private int argent;
+	private int nbProp;
 
-    public Joueur(int id) {
-        this.id = id;
-        this.argent = 0;
-        this.nbProp = 0;
-    }
+	public Joueur(int id) {
+		this.id = id;
+		this.argent = 0;
+		this.nbProp = 0;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public int getArgent() {
-        return argent;
-    }
+	public int getArgent() {
+		return argent;
+	}
 
-    public void addProp() {
-        nbProp++;
-    }
+	/**
+	 * Ajouter les propriétées de l'unité
+	 */
+	public void addProp() {
+		nbProp++;
+	}
 
-    public void addMoney() {
-        argent += nbProp * 1000;
-    }
+	/**
+	 * Augmenter l'argent d'un joueur
+	 * 
+	 */
+	public void addMoney() {
+		argent += nbProp * 1000;
+	}
 }
